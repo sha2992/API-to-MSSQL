76c262c9dc4375f5c2da9ce646dcf0ece8061da4
Shakil Ahmed <73547483+sha2992@users.noreply.github.com>
2024-05-03 11:39:03 +0600

    Add files via upload

---
+++ Extract and Load.py
@@ -0,0 +1,36 @@
+# importing libraries
+
+#pip3 install requests
+
+import requests
+import pandas
+import sqlalchemy
+
+url = 'http://api.coincap.io/v2/assets'
+
+header = {'Content-Type':'application/json',
+          'Accept-Encoding':'deflate'}
+
+response = requests.get(url,headers=header)
+
+#print(response)
+
+responseData= response.json()
+
+#print(responseData)
+
+df = pandas.json_normalize(responseData,'data')
+
+#print(df.head())
+
+server = 'DESKTOP-73ELNP5'
+driver = 'SQL Server Native Client 11.0'
+database = 'DataEngineering'
+
+conn_str = f'mssql+pyodbc://@{server}/{database}?driver={driver}'
+engine = sqlalchemy.create_engine(conn_str)
+
+#connec = engine.connect()
+
+df.to_sql(name='FactCrypto',con=engine,index=False,if_exists='replace')
+
